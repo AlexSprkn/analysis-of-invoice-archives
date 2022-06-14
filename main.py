9c98b255ee5a4b684ff3d395db5a0c183022f0e7
Alexandre <as.rg.beat@yandex.ru>
2022-06-14 13:27:08 +0300

    Удалил лишние комментарии, разделил части программы переносами для удобства чтения

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import xlrd
 import openpyxl
 
 def get_list_of_input_files(dir_with_invoices):
-    # Функция для получения списка файлов с архивами
+    '''Function for getting a list of files with archives'''
+
     input_files = listdir(dir_with_invoices)
     invoice_name = 'Архив счетов-фактур'
     input_files_temp = []
@@ -15,8 +16,8 @@ def get_list_of_input_files(dir_with_invoices):
     return input_files
 
 def get_float_from_cell(cell):
-    # Функция для извлечения значения с плавающей запятой из
-    # ячейки с запятой в качестве разделителя
+    '''function to extract floating point value from cell'''
+
     cell_value = str(cell.value)
     if ',' in cell_value:
         result = ''
@@ -36,10 +37,11 @@ def get_float_from_cell(cell):
             return None
 
 def calculate_profit(input_file, include_groups = False):
-    # Функция получает путь к файлу архива счетов-фактур, расчитывает показатели и
-    # возвращает словарь со значениями
+    '''The function receives the path to the invoice archive file,
+    calculates the indicators and returns a dictionary with values'''
+
     result = {}
-    # Пытаемся извлечь дату из названия
+    # Extracting date from file name
     can_get_date = (
         input_file.find(' с ') != -1,
         input_file.find(' по ') != -1,
@@ -47,25 +49,26 @@ def calculate_profit(input_file, include_groups = False):
     result['Invoice_date'] = None
     if all(can_get_date):
         result['Invoice_date'] = input_file[input_file.find(' с ')+1:input_file.find(' по ')+14]
-    # Открываем файл и получаем таблицу с первого листа
+    
+    # Opening the file and getting the table from the first sheet
     table = xlrd.open_workbook(input_file).sheet_by_index(0)
-    # Достаём из него колонки с наименованием и суммой
+    
+    # Getting columns with name, sum, sales volumes and optionally groups
     products = table.col(0)[3:-3]
     products_sum = table.col(3)[3:-3]
     product_sales_volumes = table.col(2)[3:-3]
     product_groups = None
     if include_groups:
         product_groups = table.col(1)[3:-3]
-    # Инициализируем словарь для информации по товарам и счётчик общей суммы продаж
+    
     result_products = {}
     total_revenue = 0
-    #С помощью цикла проходимся по строкам таблицы
-    #Наименование - product, сумма по наименованию - product_sum
+
     for index in range(len(products)):
         product = products[index].value
         product_sum = get_float_from_cell(products_sum[index])
         product_sales_volume = get_float_from_cell(product_sales_volumes[index])
-        # Пропускаем строки не с товарами
+        
         not_valid_data = (
             product.startswith('Наименование'),
             product.startswith('№'),
@@ -77,9 +80,9 @@ def calculate_profit(input_file, include_groups = False):
             )
         if any(not_valid_data):
             continue
-        # Подсчитываем общую выручку
+        
         total_revenue += product_sum
-        # Записываем полученные значения в словарь
+
         if result_products.get(product):
             result_products[product]['product_sum'] += product_sum
             result_products[product]['product_sales_volume'] += product_sales_volume
@@ -87,102 +90,117 @@ def calculate_profit(input_file, include_groups = False):
             result_products[product] = {
             'product_sum' : product_sum,
             'product_sales_volume' : product_sales_volume}
+        
         if include_groups:
             result_products[product]['group'] = product_groups[index].value
         else:
             result_products[product]['group'] = None
-    # Добавляем информацию по товарам в результат
+
     result['result_products'] = result_products
     result['total_revenue'] = total_revenue
     result['include_groups'] = include_groups
     return result
 
 def write_results(results_list):
-    # Функция принимает список с результатами и записывает их в соответствующие файлы
-    counter = 1
-    # Предустановка для границ в Excel
+    '''The function takes a list of results and writes them to the appropriate files'''
+
     bd = openpyxl.styles.Side(style='thin', color="000000")
     all_borders = openpyxl.styles.Border(left=bd, top=bd, right=bd, bottom=bd)
+    allignment_center = openpyxl.styles.Alignment(wrapText=True, horizontal='center', vertical='center')
+    bold_font = openpyxl.styles.Font(bold=True)
+
+    counter = 1
     for result in results_list:
-        # Записываем словарь с информацие по товарам в переменную
         result_products = result['result_products']
-        # Создание таблицы, заполение и форматирование ячеек, сохранение в файл
+
         wb = openpyxl.Workbook()
         ws = wb['Sheet']
+
         ws.column_dimensions['A'].width = 80
         ws.column_dimensions['C'].width = 30
         ws.column_dimensions['D'].width = 40
+        
         identificator = counter
         if result.get('Invoice_date'):
             identificator = result['Invoice_date']
+
         ws['A1'] = 'Общая выручка: '
-        ws['A1'].font = openpyxl.styles.Font(bold=True)
+        ws['A1'].font = bold_font
         ws['A1'].border = all_borders
+
         ws['A2'] = 'Приблизительная сумма прибыли при наценке 5%: '
-        ws['A2'].font = openpyxl.styles.Font(bold=True)
+        ws['A2'].font = bold_font
         ws['A2'].border = all_borders
+
         total_revenue = result['total_revenue']
         ws['C1'] = total_revenue
-        ws['C1'].alignment = openpyxl.styles.Alignment(horizontal='center', vertical='center')
+        ws['C1'].alignment = allignment_center
         ws['C1'].border = all_borders
         ws['C1'].number_format = '0.00'
+
         ws['C2'] = (total_revenue / 1.05 / 1.2) * 0.05
         ws['C2'].number_format = '0.00'
-        ws['C2'].alignment = openpyxl.styles.Alignment(horizontal='center', vertical='center')
+        ws['C2'].alignment = allignment_center
         ws['C2'].border = all_borders
+
         ws['A3'] = 'Приблизительная сумма прибыли при наценке (укажите процент): '
-        ws['A3'].font = openpyxl.styles.Font(bold=True)
+        ws['A3'].font = bold_font
         ws['A3'].border = all_borders
+
         ws['B3'] = 0
         ws['B3'].number_format = '0.00%'
+        ws['B3'].alignment = allignment_center
         ws['B1'].border = all_borders
         ws['B2'].border = all_borders
         ws['B3'].border = all_borders
+
         ws['C3'] = '=C1/1.2/(1+B3)*B3'
         ws['C3'].number_format = '0.00'
-        ws['C3'].alignment = openpyxl.styles.Alignment(horizontal='center', vertical='center')
+        ws['C3'].alignment = allignment_center
         ws['C3'].border = all_borders
+
         ws['A5'] = 'Товар'
-        ws['A5'].font = openpyxl.styles.Font(bold=True)
-        ws['A5'].alignment = openpyxl.styles.Alignment(wrapText=True, horizontal='center', vertical='center')
+        ws['A5'].font = bold_font
+        ws['A5'].alignment = allignment_center
         ws['A5'].border = all_borders
+
         ws['B5'] = 'Продано единиц'
-        ws['B5'].font = openpyxl.styles.Font(bold=True)
-        ws['B5'].alignment = openpyxl.styles.Alignment(wrapText=True, horizontal='center', vertical='center')
+        ws['B5'].font = bold_font
+        ws['B5'].alignment = allignment_center
         ws['B5'].border = all_borders
+
         ws['C5'] = f'Сумма продаж за период {identificator} по товару'
-        ws['C5'].font = openpyxl.styles.Font(bold=True)
-        ws['C5'].alignment = openpyxl.styles.Alignment(wrapText=True, horizontal='center', vertical='center')
+        ws['C5'].font = bold_font
+        ws['C5'].alignment = allignment_center
         ws['C5'].border = all_borders
+
         if result['include_groups']:
             ws['D5'] = "Группа товара"
-            ws['D5'].font = openpyxl.styles.Font(bold=True)
-            ws['D5'].alignment = openpyxl.styles.Alignment(wrapText=True, horizontal='center', vertical='center')
+            ws['D5'].font = bold_font
+            ws['D5'].alignment = allignment_center
             ws['D5'].border = all_borders
         
-        # Сортировка товаров по убыванию суммы продаж
         result_products_sorted = list(result_products.keys())
         result_products_sorted.sort(key=lambda product: result_products[product]['product_sum'], reverse=True)
 
-
-        # Запись значений в таблицу
         for index, product in enumerate(result_products_sorted):
             ws['A'+str(index+6)] = product
-            ws['A'+str(index+6)].alignment = openpyxl.styles.Alignment(vertical='center')
             ws['A'+str(index+6)].border = all_borders
+
+            ws['B'+str(index+6)] = result_products[product]['product_sales_volume']
+            ws['B'+str(index+6)].alignment = allignment_center
+            ws['B'+str(index+6)].border = all_borders
+
             ws['C'+str(index+6)] = result_products[product]['product_sum']
             ws['C'+str(index+6)].number_format = '0.00'
-            ws['C'+str(index+6)].alignment = openpyxl.styles.Alignment(horizontal='center', vertical='center')
+            ws['C'+str(index+6)].alignment = allignment_center
             ws['C'+str(index+6)].border = all_borders
-            ws['B'+str(index+6)] = result_products[product]['product_sales_volume']
-            ws['B'+str(index+6)].alignment = openpyxl.styles.Alignment(horizontal='center', vertical='center')
-            ws['B'+str(index+6)].border = all_borders
+
             if result['include_groups']:
                 ws['D'+str(index+6)] = result_products[product]['group']
-                ws['D'+str(index+6)].alignment = openpyxl.styles.Alignment(vertical='center')
+                ws['D'+str(index+6)].alignment = allignment_center
                 ws['D'+str(index+6)].border = all_borders
         
-        # Сохраняем в файл
         output_file = f'Отчёт по продажам {identificator}.xlsx'
         output_file = join('Отчёты', output_file)
         wb.save(output_file)
@@ -190,16 +208,15 @@ def write_results(results_list):
 
 
 def main():
-    # Создаём папку для архивов, если её нет
     dir_with_invoices = join(dirname(__file__), 'Архивы счетов-фактур')
     if not exists(dir_with_invoices):
         mkdir(dir_with_invoices)
-        print('''Создана папка для архивов счетов-фактур.
-Перенесите архивы в папку "Архивы счетов-фактур",
-чтобы сформировать отчёты о продажах.''')
+        print('Создана папка для архивов счетов-фактур.\n'+
+            'Перенесите архивы в папку "Архивы счетов-фактур",\n' +
+            'чтобы сформировать отчёты о продажах.')
         input('Для продолжения нажмите Enter')
+    
     empty_dir = True
-    # Проверяем, что папка с архивами не пустая
     while empty_dir:
         empty_dir = not bool(listdir(dir_with_invoices))
         if empty_dir:
@@ -207,25 +224,24 @@ def main():
                 'Перенесите архивы в папку "Архивы счетов-фактур", чтобы сформировать отчёты о продажах,\n' +
                 'и нажмите Enter')
 
-    # Когда в папке есть архивы, получаем их имена
     print('Получение списка архивов счетов-фактур...')
     input_files = get_list_of_input_files(dir_with_invoices)
-    # Создаём список для результатов по архивам и счётчик для отображения прогресса
+
     results = []
     counter = 1
     number_of_files = len(input_files)
-    # Для каждого файла производим вычисления и записываем в список результатов в виде словаря
+
     for file in input_files:
         print(f'Рассчитываем {counter} отчёт из {number_of_files}...')
         input_file_path = join(dir_with_invoices, file)
         results.append(calculate_profit(input_file_path, include_groups=True))
         counter += 1
-    # Проверяем наличие папки для сохранения отчётов. Если нет - создаём
+
     print('Сохраняем отчёты в Excel...')
     dir_to_save_results = join(dirname(__file__), 'Отчёты')
     if not exists(dir_to_save_results):
         mkdir(dir_to_save_results)
-    # Записываем результаты в файлы Excel с указанием периода в наименовании
+
     write_results(results)
     print('Отчёты о продажах сохранены в папку "Отчёты".')
     input('Для завершения нажмите Enter')
